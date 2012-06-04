32df2c086f033dd4bc7c09275b5ac64ca93fecb4
Kyle Finley <kyle.finley@gmail.com>
2012-06-03 21:33:30 -0500

    Added LoginRequired and AdminRequired http wrappers.

--- user/http.go
+++ user/http.go
@@ -4,44 +4,43 @@
 
 package user
 
-//import (
-//	"appengine"
-//	"github.com/scotch/hal/session"
-//	"net/http"
-//)
-//
-//var config = map[string]string{
-//	"login_url": "/auth/login",
-//}
-//
-//// LoginRequired is a wrapper for http.HandleFunc. If the requesting
-//// User is not logged in, they will be redirect to the login page.
-//func LoginRequired(fn http.HandlerFunc) http.HandlerFunc {
-//	return func(w http.ResponseWriter, r *http.Request) {
-//		userid := CurrentUserID(r)
-//		if userid == 0 {
-//			http.Redirect(w, r, config["login_url"], http.StatusFound)
-//		}
-//		fn(w, r)
-//	}
-//}
-//
-//// AdminRequired is a wrapper for http.HandleFuc. It the requesting 
-//// User is not an admin, they will redirect to the login page.
-//func AdminRequired(fn http.HandlerFunc) http.HandlerFunc {
-//	return func(w http.ResponseWriter, r *http.Request) {
-//		c := appengine.NewContext(r)
-//		// Get the session
-//		session, err := session.Store.Get(r, "auth")
-//		if err != nil {
-//			c.Errorf("There was an error retrieving the session: %v", err)
-//		}
-//		check(err)
-//		userid, _ := session.Values["userid"].(int64)
-//		isAdmin, _ := session.Values["isadmin"].(int64)
-//		if userid == 0 || isAdmin == 0 {
-//			http.Redirect(w, r, config["login_url"], http.StatusFound)
-//		}
-//		fn(w, r)
-//	}
-//}
+import (
+	"appengine"
+	"github.com/scotch/hal/session"
+	"net/http"
+)
+
+var config = map[string]string{
+	"login_url": "/-/auth/login",
+}
+
+// LoginRequired is a wrapper for http.HandleFunc. If the requesting
+// User is not logged in, they will be redirect to the login page.
+func LoginRequired(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		userid, _ := CurrentUserID(r)
+		if userid == 0 {
+			http.Redirect(w, r, config["login_url"], http.StatusFound)
+		}
+		fn(w, r)
+	}
+}
+
+// AdminRequired is a wrapper for http.HandleFuc. It the requesting
+// User is not an admin, they will redirect to the login page.
+func AdminRequired(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c := appengine.NewContext(r)
+		// Get the session
+		sess, err := session.Store.Get(r, "auth")
+		if err != nil {
+			c.Errorf("There was an error retrieving the session: %v", err)
+		}
+		userid, _ := sess.Values["userid"].(int64)
+		isAdmin, _ := sess.Values["isadmin"].(int64)
+		if userid == 0 || isAdmin == 0 {
+			http.Redirect(w, r, config["login_url"], http.StatusFound)
+		}
+		fn(w, r)
+	}
+}
