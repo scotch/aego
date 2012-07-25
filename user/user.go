3cb091f0aa8c67aabb2c6e01e09278aac9f242a9
Kyle Finley <kyle.finley@gmail.com>
2012-07-25 12:47:38 -0500

    user: organized code

--- user/user.go
+++ user/user.go
@@ -31,14 +31,14 @@ type User struct {
 	Password []byte
 	// Roles is a list of role names that the user belongs to.
 	Roles []string
-	// Created is a time.Time of when the User was first created.
-	Created time.Time
-	// Updated is a time.Time of when the User was last updated.
-	Updated time.Time
 	// Person is an Object representing personal information about the user.
 	Person *person.Person `datastore:"-"`
 	// PersonJSON is the Person object converted to JSON, for storage purposes.
 	PersonJSON []byte `datastore:"Person"`
+	// Created is a time.Time of when the User was first created.
+	Created time.Time
+	// Updated is a time.Time of when the User was last updated.
+	Updated time.Time
 }
 
 // New creates a new user and set the Created to now
@@ -50,16 +50,22 @@ func New() *User {
 	}
 }
 
-func (u *User) Decode() error {
-	if u.PersonJSON != nil {
-		var p *person.Person
-		err := json.Unmarshal(u.PersonJSON, &p)
-		u.Person = p
-		return err
+// SetKey creates and embeds a ds.Key into the entity.
+func (u *User) SetKey(c appengine.Context) {
+	// If we are saving for the first time lets get an id so that we
+	// can save the id to the json data before saving the entity. This
+	// prevents us from having to save twice.
+	if u.Key == nil || u.Key.StringID() == "" {
+		id, err := ds.AllocateID(c, "User")
+		if err != nil {
+			return err
+		}
+		u.Key = datastore.NewKey(c, "User", id, 0, nil)
 	}
-	return nil
 }
 
+// Encode is called prior to save. Any fields that need to be updated
+// prior to save are updated here.
 func (u *User) Encode() error {
 	// Update Person
 
@@ -86,27 +92,15 @@ func (u *User) Encode() error {
 	return err
 }
 
-// Put is a convience method to save the User to the datastore and
-// updated the Updated property to time.Now(). This method should
-// always be usdd when saving a user, fore it does some necessary
-// preprocessing.
-func (u *User) Put(c appengine.Context) error {
-
-	// If we are saving for the first time lets get an id so that we
-	// can save the id to the json data before saving the entity. This
-	// prevents us from having to save twice.
-	if u.Key == nil || u.Key.StringID() == "" {
-		id, err := ds.AllocateID(c, "User")
-		if err != nil {
-			return err
-		}
-		u.Key = datastore.NewKey(c, "User", id, 0, nil)
+// Decode is called after the entity has been retrieved from the the ds.
+func (u *User) Decode() error {
+	if u.PersonJSON != nil {
+		var p *person.Person
+		err := json.Unmarshal(u.PersonJSON, &p)
+		u.Person = p
+		return err
 	}
-	u.Updated = time.Now()
-	u.Encode()
-	key, err := ds.Put(c, u.Key, u)
-	u.Key = key
-	return err
+	return nil
 }
 
 // Get is a convience method for retrieveing an entity foom the store.
@@ -117,4 +111,17 @@ func Get(c appengine.Context, id string) (u *User, err error) {
 	u.Key = key
 	u.Decode()
 	return u, err
+}
+
+// Put is a convience method to save the User to the datastore and
+// updated the Updated property to time.Now(). This method should
+// always be usdd when saving a user, fore it does some necessary
+// preprocessing.
+func (u *User) Put(c appengine.Context) error {
+	u.SetKey()
+	u.Encode()
+	u.Updated = time.Now()
+	key, err := ds.Put(c, u.Key, u)
+	u.Key = key
+	return err
 }
