8fac671ccad0840c11052048f93cbca30aef37b3
Kyle Finley <kyle.finley@gmail.com>
2012-08-10 17:38:08 -0500

    email: changed email validation to a regex thank you Go-Start - forgot license

--- email/email.go
+++ email/email.go
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Copyright 2012 Erik Unger. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
 /*
 Package hal/email provides methods surronding:
 
